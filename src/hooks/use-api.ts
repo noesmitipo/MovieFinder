7a8e72603f545398c3578d5b80252ae9be443dac
Sergio Ferrer <sergio.ferrer@purple.nl>
2023-02-15 15:42:28 +0100

    Handle error from getMovieDetails

--- src/hooks/use-api.ts
+++ src/hooks/use-api.ts
@@ -27,10 +27,10 @@ export const useApi = () => {
 
   const getMovieDetails = async (id: string): Promise<MovieDetails> => {
     const response = await fetch(`${API_URL}movie/${id}?api_key=${API_KEY}`);
-
-    // if (!response.ok) {
-    //   return {};
-    // }
+    console.log(response);
+    if (!response.ok) {
+      throw Error("Movie not found");
+    }
 
     const data = await response.json();
     return data as MovieDetails;
--- src/pages/movie-details.tsx
+++ src/pages/movie-details.tsx
@@ -1,36 +1,56 @@
 import { Image, Space } from "antd";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useApi } from "../hooks/use-api";
 import { MovieDetails } from "../interfaces/movie-details";
 
-const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+const IMAGE_PATH = import.meta.env.VITE_IMAGE_PATH;
 
 export const MovieDetail = () => {
   const params = useParams();
   const id = params.movieId;
   const api = useApi();
   const [movie, setMovie] = useState<MovieDetails>();
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    if (id === undefined) return;
+    if (id === undefined) {
+      setError(true);
+      return;
+    }
 
     const fetchMovie = async () => {
-      const movie = await api.getMovieDetails(id);
-      setMovie(movie);
+      try {
+        const movie = await api.getMovieDetails(id);
+        setMovie(movie);
+        setError(false);
+      } catch (error) {
+        setError(true);
+      }
     };
 
     fetchMovie();
   }, []);
 
   return (
-    <Space direction="vertical">
-      <Image width={300} src={IMAGE_PATH + movie?.poster_path}></Image>
-      <div
-        style={{ fontWeight: "bold" }}
-      >{`${movie?.title} (${movie?.release_date})`}</div>
-      <div>{movie?.overview}</div>
-      <div>Genres: {movie?.genres.map((genre) => genre.name + " ")}</div>
-    </Space>
+    <>
+      {error && <div>Movie not found</div>}
+      {!error && (
+        <Space direction="vertical">
+          <Image width={300} src={IMAGE_PATH + movie?.poster_path}></Image>
+          <div
+            style={{ fontWeight: "bold" }}
+          >{`${movie?.title} (${movie?.release_date})`}</div>
+          <div>{movie?.overview}</div>
+          <div>
+            Genres:{" "}
+            {movie?.genres.map(
+              (genre, index) =>
+                genre.name + (index < movie.genres.length - 1 ? ", " : "")
+            )}
+          </div>
+        </Space>
+      )}
+    </>
   );
 };
